ae3774558038785b34b6fd9c3556da42f1cd6550
ArceusLegend <markoskap13@gmail.com>
2025-01-04 18:45:34 +0200

    Update backend requirements to allow connection from a front end app

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -47,6 +47,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    # From django-cors-headers
+    "corsheaders",
 ]
 
 MIDDLEWARE = [
@@ -57,6 +59,13 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    # From django-cors-headers
+    "corsheaders.middleware.CorsMiddleware",
+]
+
+UI_BIND_PORT = os.getenv("UI_BIND_PORT", 3000)
+CORS_ORIGIN_WHITELIST = [
+    f"http://localhost:{UI_BIND_PORT}",  # The default port for create-react-app
 ]
 
 ROOT_URLCONF = "ecommerce.urls"
